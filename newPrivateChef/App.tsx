1006ba526d829235b367c655f9ac5c7967f56356
LikhonaNdabezitha039 <zululikhona200@gmail.com>
2025-11-13 12:48:36 +0200

    here i moved added the folder of the images to the project.and i also added the imports that i will need for important libraries on the app . i also added the type of data types.

---
+++ newPrivateChef/App.tsx
@@ -0,0 +1,29 @@
+//The import of the important libraries on the app
+import React, { useMemo, useState, createContext, useContext } from "react";
+import { StyleSheet, Text, View, TextInput, Button, FlatList,
+         ScrollView, Image, Keyboard } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Picker } from "@react-native-picker/picker";
+import Ionicons from "@expo/vector-icons/Ionicons"; //For the icons
+import { StatusBar } from "expo-status-bar";
+
+//Types of data structures
+//A single menu item stored in the catalogue
+type StoreItem = {
+  id: string;
+  name: string;
+  price: number; // numeric price (in Rands)
+  category: "Starters" | "Main Courses" | "Desserts";
+  description: string;
+  image: any; 
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
